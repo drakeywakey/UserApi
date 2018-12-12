5023791b05d72439dc221628a1f7ceaf41b0c23f
Drake <dbennion@varomoney.com>
2018-12-11 19:44:20 -0700

    add User model

---
+++ UserApi/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UserApi.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public List<string> Interests { get; set; }
+        public int Age { get; set; }
+        public string ImageSrc { get; set; }
+    }
+}
