f2a746c75efd2b015aec89c5fc972080b6297363
Drake <dbennion@varomoney.com>
2018-12-16 16:09:22 -0700

    another unit test

--- UserApiTests/UserControllerTest.cs
+++ UserApiTests/UserControllerTest.cs
@@ -12,12 +12,40 @@ namespace UserApiTests
     public class UserControllerTest
     {
         UserController _controller;
+        Mock<IUserService> _service;
 
         public UserControllerTest()
         {
-            var mockService = new Mock<IUserService>();
-            mockService.Setup(service => service.GetAllUsers()).Returns(new List<User>());
-            _controller = new UserController(mockService.Object);
+            _service = new Mock<IUserService>();
+
+            var list = new List<User>
+            {
+                new User
+                {
+                    Id = 1,
+                    FirstName = "Doug",
+                    LastName = "Dockson",
+                    Address = "Murray",
+                    Age = 39,
+                    ImageSrc = "src",
+                    Interests = "So much interest"
+                },
+
+                new User
+                {
+                    Id = 2,
+                    FirstName = "Carl",
+                    LastName = "Boon",
+                    Address = "Massachusetts",
+                    Age = 26,
+                    ImageSrc = "src",
+                    Interests = "Sports"
+                }
+            };
+
+            _service.Setup(service => service.GetAllUsers()).Returns(list);
+
+            _controller = new UserController(_service.Object);
         }
 
         [Fact]
@@ -25,6 +53,14 @@ namespace UserApiTests
         {
             var result = _controller.GetAll();
             Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void GetAll_ReturnsAllUsers()
+        {
+            var result = _controller.GetAll().Result as OkObjectResult;
+            var items = Assert.IsType<List<User>>(result.Value);
+            Assert.Equal(2, items.Count);  
         }
     }
 }
