2045433c8e36aaca1ab216b590a495d6cf7cfd42
Drake <dbennion@varomoney.com>
2018-12-16 16:40:10 -0700

    a bit of test refactoring

--- UserApiTests/UserControllerTest.cs
+++ UserApiTests/UserControllerTest.cs
@@ -17,34 +17,6 @@ namespace UserApiTests
         public UserControllerTest()
         {
             _service = new Mock<IUserService>();
-
-            var list = new List<User>
-            {
-                new User
-                {
-                    Id = 1,
-                    FirstName = "Doug",
-                    LastName = "Dockson",
-                    Address = "Murray",
-                    Age = 39,
-                    ImageSrc = "src",
-                    Interests = "So much interest"
-                },
-
-                new User
-                {
-                    Id = 2,
-                    FirstName = "Carl",
-                    LastName = "Boon",
-                    Address = "Massachusetts",
-                    Age = 26,
-                    ImageSrc = "src",
-                    Interests = "Sports"
-                }
-            };
-
-            _service.Setup(service => service.GetAllUsers()).Returns(list);
-
             _controller = new UserController(_service.Object);
         }
 
@@ -53,14 +25,6 @@ namespace UserApiTests
         {
             var result = _controller.GetAll();
             Assert.IsType<OkObjectResult>(result.Result);
-        }
-
-        [Fact]
-        public void GetAll_ReturnsAllUsers()
-        {
-            var result = _controller.GetAll().Result as OkObjectResult;
-            var items = Assert.IsType<List<User>>(result.Value);
-            Assert.Equal(2, items.Count);  
         }
     }
 }
---
+++ UserApiTests/UserServiceTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using UserApi.Infrastructure;
+using UserApi.Interfaces;
+using UserApi.Models;
+using UserApi.Services;
+using Xunit;
+
+namespace UserApiTests
+{
+    public class UserServiceTest
+    {
+        IUserService _service;
+        Mock<IUserRepository> _repository;
+
+        public UserServiceTest()
+        {
+            _repository = new Mock<IUserRepository>();
+
+            var list = new List<User>
+            {
+                new User
+                {
+                    Id = 1,
+                    FirstName = "Doug",
+                    LastName = "Dockson",
+                    Address = "Murray",
+                    Age = 39,
+                    ImageSrc = "src",
+                    Interests = "So much interest"
+                },
+
+                new User
+                {
+                    Id = 2,
+                    FirstName = "Sylvia",
+                    LastName = "Plat",
+                    Address = "Germany",
+                    Age = 44,
+                    ImageSrc = "src",
+                    Interests = "Poetry"
+                },
+
+                new User
+                {
+                    Id = 3,
+                    FirstName = "Walt",
+                    LastName = "White",
+                    Address = "New Mexico",
+                    Age = 56,
+                    ImageSrc = "src",
+                    Interests = "Meth"
+                },
+
+                new User
+                {
+                    Id = 4,
+                    FirstName = "Carl",
+                    LastName = "Boon",
+                    Address = "Massachusetts",
+                    Age = 26,
+                    ImageSrc = "src",
+                    Interests = "Sports"
+                },
+
+                new User
+                {
+                    Id = 5,
+                    FirstName = "Penny",
+                    LastName = "Shelstorp",
+                    Address = "Canada",
+                    Age = 19,
+                    ImageSrc = "src",
+                    Interests = "Pink"
+                }
+            };
+
+            _repository.Setup(repo => repo.GetAll()).Returns(list);
+            _service = new UserService(_repository.Object);
+        }
+
+        [Fact]
+        public void GetAll_ReturnsAllUsers()
+        {
+            Assert.Equal(5, _service.GetAllUsers().Count());
+        }
+    }
+}
