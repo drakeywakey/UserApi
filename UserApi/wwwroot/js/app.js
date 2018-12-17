615b1e1ca624ae376095adda662b59f33f2b9b7b
Drake <dbennion@varomoney.com>
2018-12-17 13:59:06 -0700

    finished simple UI for search. up next is probably simulated delay

--- UserApi/wwwroot/js/app.js
+++ UserApi/wwwroot/js/app.js
@@ -1,22 +1,70 @@
-﻿class Home extends React.Component {
+﻿const baseUrl = 'https://localhost:5001/api/user'
 
-    state = {
-        persons: []
+class Person extends React.Component {
+
+    render() {
+        const { firstName, lastName } = this.props.person
+        return (
+            <li>{firstName + ' ' + lastName}</li>
+        )
     }
 
-    componentDidMount() {
-        axios.get('https://localhost:5001/api/user')
-            .then(res => {
-                const persons = res.data
-                this.setState({ persons })
-            })
+}
+
+class Search extends React.Component {
+
+    state = {
+        search: ''
+    }
+
+    handleChange(event) {
+        this.setState({
+            search : event.target.value
+        })
     }
 
     render() {
         return (
-            <ul>
-                { this.state.persons.map(person => <li key={ person.id }>{ person.firstName }</li>) }
-            </ul>
+            <div>
+                <input onChange={this.handleChange.bind(this)} value={ this.state.search }/>
+                <button onClick={ () => this.props.handleClick(this.state.search) }>Search!</button>
+            </div>
+        )
+    }
+}
+
+class Home extends React.Component {
+
+    state = {
+        persons: [],
+    }
+
+    loadUsers() {
+        axios.get(baseUrl)
+            .then(res => this.setState({ persons : res.data }))
+    }
+
+    searchUsers(search) {
+        if (search) {
+            axios.get(baseUrl + '/search?search=' + search)
+                .then(res => this.setState({ persons : res.data }))
+        } else {
+            this.loadUsers()
+        }
+    }
+
+    componentDidMount() {
+        this.loadUsers()
+    }
+
+    render() {
+        return (
+            <div>
+                <Search handleClick={this.searchUsers.bind(this)} />
+                <ul>
+                    { this.state.persons.map(person => <Person key={ person.id } person={ person } />) }
+                </ul>
+            </div>
         )
     }
 }
